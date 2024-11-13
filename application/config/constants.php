33b4b54f1f41287d33d74734baf80e1556b35bd7
marceloaudisioClases <marcelo@hilet.com>
2024-11-12 21:24:35 -0300

    Agregue el parametro para usuario
    Co-Authored-By: Dragonbenja <185248328+Dragonbenja@users.noreply.github.com>

--- application/config/constants.php
+++ application/config/constants.php
@@ -88,4 +88,5 @@ defined('EXIT__AUTO_MAX')      OR define('EXIT__AUTO_MAX', 125); // highest auto
 //Mis parámetros
 
 defined('TIPO_REGISTRO')      OR define('TIPO_REGISTRO', 2); // 1:abierto,2:Pend Aprobacion,3:Cerrado
-defined('ROL_ADMIN')      OR define('ROL_ADMIN', 1); // 1:abierto,2:Pend Aprobacion,3:Cerrado
+defined('ROL_ADMIN')      OR define('ROL_ADMIN', 1); 
+defined('ROL_USER')      OR define('ROL_USER', 2); 
