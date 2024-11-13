361170fac27715b452d853c9b977e3b3bc561fc3
Dragonbenja <calvo.balzola.benjamin@gmail.com>
2024-11-12 21:27:49 -0300

    Cree la view de usuarios

---
+++ application/views/usuarios/usuarios.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bienvenida de usuarios</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">  
+  </head>
+  <body>
+    <?php $this->load->view("menu"); ?>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col">
+              
+                <h1 class="display-1">Lista de usuarios</h1>
+
+                <div class="card">
+                    <div class="card-body">
+                    <?php foreach($categorias as $c){ ?>   
+                  <a href="#" class="list-group-item list-group-item-action"><?php echo $c["usuario"]; ?></a>
+               <?php } ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
