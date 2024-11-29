821de148e37394bd7b14231c5c44bc6e6ce6c0ef
marceloaudisioClases <marcelo@hilet.com>
2024-11-28 21:20:49 -0300

    Update principal.php

--- application/views/catalogo/principal.php
+++ application/views/catalogo/principal.php
@@ -39,6 +39,33 @@
                     <h1 class="display-3"><i class="bi bi-search"></i> Buscando: "<?php echo $buscar; ?>"</h1>
                   <?php }else{ ?>
                     <h1 class="display-3">Portada</h1>
+                    <div class="col-md-6 offset-md-3">
+                    <div id="carouselExampleIndicators" class="carousel slide">
+                    <div class="carousel-indicators">
+                      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                      
+                    </div>
+                    <div class="carousel-inner">
+                      <div class="carousel-item active">
+                        <img src="<?php echo base_url("img/1.png"); ?>" class="d-block w-100" alt="...">
+                      </div>
+                      <div class="carousel-item">
+                        <img src="<?php echo base_url("img/2.png"); ?>" class="d-block w-100" alt="...">
+                      </div>
+                      
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                      <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                      <span class="visually-hidden">Next</span>
+                    </button>
+                  </div>
+                    </div>
+                    
                   <?php } ?>
                 <?php } ?>
                 <br>
