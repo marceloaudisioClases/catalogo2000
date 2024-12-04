151f6ca551f6e9f2e74cfd7a674ce5326811772f
FrancoRioos <riosfranco66@gmail.com>
2024-12-04 04:02:47 -0300

    listo

--- application/views/menu.php
+++ application/views/menu.php
@@ -19,12 +19,8 @@
             </a>
             <ul class="dropdown-menu">
                 <li><a class="dropdown-item" href="<?php echo site_url("usuarios/index"); ?>">Usuarios</a></li>
-<<<<<<< Updated upstream
                 <li><a class="dropdown-item" href="#">Roles</a></li>
                 <li><a class="dropdown-item" href="#">Categorias</a></li>
-=======
-                <li><a class="dropdown-item" href="<?php echo site_url("roles/index"); ?>">Roles</a></li>
->>>>>>> Stashed changes
                 <li><hr class="dropdown-divider"></li>
                 <li><a class="dropdown-item" href="<?php echo site_url("productos"); ?>">Productos</a></li>
             </ul>
