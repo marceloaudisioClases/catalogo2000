50cfa7c30e6217ef144f345f228da478148668db
1201Cris <abrahamcris@hotmail.com.ar>
2024-12-07 00:32:09 -0300

    cambios

--- application/views/login.php
+++ application/views/login.php
@@ -6,7 +6,8 @@
     <title>Login de usuarios</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-    <link rel="stylesheet" href="<?= base_url('style.css') ?>">
+  
+   rel="stylesheet" href="<?= base_url('style.css') ?>">
   </head>
   <body>
     <div class="body">
